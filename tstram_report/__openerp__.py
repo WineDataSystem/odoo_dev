c829fc1ec0d786b73dc00b95f57fcaf61f7f71dc
PIPONP <philippe@winedatasystem.com>
2016-03-15 14:59:27 +0100

    premier

---
+++ tstram_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Tstram Report',
+    'category': 'Uncategorized',
+    'summary': 'Tstram Report',
+    'website': 'winedatasystem.com',
+    'version': '1.0',
+    'description': """
+Tstram Report
+        """,
+    'author': 'Philippe Pipon',
+    'depends': ['base', 'website'],
+    'installable': True,
+    'data': [
+        'report/tstram_view.xml',
+        'report/website_templates.xml'
+    ],
+    'demo': [
+    ],
+    'application': True,
+}
---
+++ tstram_report/report/tstram.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import fields,osv
+from openerp import tools
+
+
+class tstram_report(osv.osv):
+    _name = "tstram.report"
+    _description = "Tstram Report"
+    _auto = False
+    _rec_name = 'date_invoice'
+    _columns = {
+        'date_due': fields.date('Order Date'),
+        'partner_id': fields.many2one('res.partner', 'Supplier'),
+        'type': fields.selection([
+            ('out_invoice', 'Customer Invoice'),
+            ('in_invoice', 'Supplier Invoice'),
+            ('out_refund', 'Customer Refund'),
+            ('in_refund', 'Supplier Refund'),
+        ], string='Type'),
+        'four_mnt2014': fields.float('Montant Fournisseurs 2014'),
+        'four_mnt2015': fields.float('Montant Fournisseurs 2015'),
+        'cli_mnt2014': fields.float('Montant Clients 2014'),
+        'cli_mnt2015': fields.float('Montant Clients 2015'),
+        'date_invoice': fields.date('Order Date'),
+    }
+    
+    _order = 'date_invoice desc'
+    
+    def init(self, cr):
+        tools.sql.drop_view_if_exists(cr, 'tstram_report')
+        cr.execute("""
+            create or replace view tstram_report as (
+                SELECT
+                  i.date_due,
+                  i.partner_id,
+                  i.type,
+                  case when(i.type='in_invoice' and i.date_invoice >= '2014-01-01' and i.date_invoice <= '2014-12-31' ) then i.amount_total else 0 end four_mnt2014,
+                  case when(i.type='in_invoice' and i.date_invoice >= '2015-01-01' and i.date_invoice <= '2015-12-31' ) then i.amount_total else 0 end four_mnt2015,
+                  case when(i.type='out_invoice' and i.date_invoice >= '2014-01-01' and i.date_invoice <= '2014-12-31' ) then i.amount_total else 0 end cli_mnt2014,
+                  case when(i.type='out_invoice' and i.date_invoice >= '2015-01-01' and i.date_invoice <= '2015-12-31' ) then i.amount_total else 0 end cli_mnt2015,
+                  i.date_invoice
+                FROM
+                  public.account_invoice i)
+        """)
