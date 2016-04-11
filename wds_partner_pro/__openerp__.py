6b0a62f0b13d96a2d40fa3ea22cf83d9601f5b20
Aymeric Fournier <proscion@gmail.com>
2016-04-11 23:40:26 +0200

    modif partner pro et partner name

--- wds_partner_pro/__openerp__.py
+++ wds_partner_pro/__openerp__.py
@@ -8,31 +8,14 @@
     'description': """
         Ce module permet d'ajouter des champs pour les professionnels
     """,
-
     'author': "WineDataSystem",
     'website': "http://www.winedatasystem.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Test',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'category': 'CRM',
+    'version': '0.2',
+    'depends': ['base','crm'],
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'security/security.xml',
-        # 'security/ir.model.access.csv',
-        # 'templates.xml',
-        # 'views/openacademy.xml',
         'views/partner.xml',
-        # 'views/session_workflow.xml',
-        # 'reports.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
 
     ],
