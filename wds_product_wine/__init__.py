be81e862e544b8ffa91efe6d247e3a455c430476
Aymeric Fournier <proscion@gmail.com>
2016-04-28 18:41:35 +0200

    ajout de wds_product_wine

--- wds_product_wine/__init__.py
+++ wds_product_wine/__init__.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
-# import controllers
-# import models
-# import partner
-# import wizard
 
-import models
+import appellation
+import vintage
+import wine
+import brand
+import offers
+# import import_product#From iframe
+#import import_offer
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
